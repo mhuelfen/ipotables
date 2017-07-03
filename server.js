3f8e1ec4d8308fb178846eac4f3724fc53144571
mhuelfen <huelfe@gmx.de>
2017-07-03 16:11:04 +0200

    make neo4j url configurable

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ var modules = require('./routes/module');
 var api = require('./routes/api');
 
 var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://localhost:7474');
+
+var db = new neo4j.GraphDatabase(
+    process.env['NEO4J_URL'] ||
+    process.env['GRAPHENEDB_URL'] ||
+    'http://localhost:7474'
+);
 
 var app = express();
 
